6f90561f0821516785bcfd9fae1bc52127b8f2ea
happydhKim <happytime870616@gmail.com>
2022-02-21 22:30:18 +0900

    [Feature] FullScreen page fullscreenRef 적용 - #11

--- src/pages/Fullscreen.tsx
+++ src/pages/Fullscreen.tsx
@@ -5,7 +5,7 @@ import { Button } from 'components';
 import type { FC } from 'react';
 
 const Fullscreen: FC = () => {
-  const { toggle, isFullscreen } = useFullscreen();
+  const { fullscreenRef, toggle, isFullscreen } = useFullscreen();
   return (
     <>
       <div
@@ -13,6 +13,16 @@ const Fullscreen: FC = () => {
           padding: 50px;
         `}
       >
+        <div
+          ref={fullscreenRef}
+          css={css`
+            font-size: 80px;
+            text-align: center;
+          `}
+        >
+          🐝
+        </div>
+        <span>If you don't use fullscreenRef, the entire screen will be fullscreen.</span>
         <Button onClick={toggle}>{isFullscreen ? 'set normal mode' : 'set fullscreen mode'}</Button>
       </div>
     </>
