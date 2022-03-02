c161b3142bedb9650e716109ea8b742776dc55d2
happydhKim <happytime870616@gmail.com>
2022-03-02 22:48:58 +0900

    [Feature] Spring -> CountUp 으로 변경 - #21

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -6,7 +6,7 @@ import type { FC } from 'react';
 
 const MainPage = lazy(() => import('pages/Main'));
 const Fullscreen = lazy(() => import('pages/Fullscreen'));
-const Spring = lazy(() => import('pages/Spring'));
+const CountUp = lazy(() => import('pages/CountUp'));
 const PageLeave = lazy(() => import('pages/PageLeave'));
 const NotFound = lazy(() => import('pages/NotFound'));
 
@@ -18,7 +18,7 @@ const Routes: FC = () => {
           <ReactRoutes>
             <Route path="/" element={<MainPage />} />
             <Route path="/fullscreen" element={<Fullscreen />} />
-            <Route path="/spring" element={<Spring />} />
+            <Route path="/countup" element={<CountUp />} />
             <Route path="/pageleave" element={<PageLeave />} />
             <Route path="*" element={<NotFound />} />
           </ReactRoutes>
